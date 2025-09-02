06290a42693f9799f74f2849b36763ee4b48157a
erlmek <erlm@ek.dk>
2025-09-02 10:50:44 +0200

    Nu med post og put

--- src/main/java/org/example/jpastudente24a/restcontroller/StudentRestController.java
+++ src/main/java/org/example/jpastudente24a/restcontroller/StudentRestController.java
@@ -3,10 +3,12 @@ package org.example.jpastudente24a.restcontroller;
 import org.example.jpastudente24a.model.Student;
 import org.example.jpastudente24a.repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class StudentRestController {
@@ -20,6 +22,57 @@ public class StudentRestController {
         List<Student> students = studentRepository.findAll();
         return students;
     }
+
+    @PostMapping("/student")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student addStudent(@RequestBody Student student) {
+        System.out.println(student);
+        Student std = studentRepository.save(student);
+        return std;
+    }
+
+    @PutMapping("/studentx")
+    public Student changeStudentx(@RequestBody Student student) {
+        System.out.println(student);
+        return studentRepository.save(student);
+    }
+
+    @PutMapping("/studentxx")
+    public Student changeStudentxx(@RequestBody Student student) {
+        Optional<Student> orgStudent = studentRepository.findById(student.getId());
+        if (orgStudent.isPresent()) {
+            return studentRepository.save(student);
+        }  else {
+            Student newStudent = new Student();
+            newStudent.setName("Not Found");
+            return newStudent;
+        }
+    }
+
+    @PutMapping("/studentxy")
+    public ResponseEntity<Student> changeStudentxy(@RequestBody Student student) {
+        Optional<Student> orgStudent = studentRepository.findById(student.getId());
+        if (orgStudent.isPresent()) {
+            studentRepository.save(student);
+            return ResponseEntity.ok(student);
+        }  else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/student/{id}")
+    public ResponseEntity<Student> changeStudent(@PathVariable int id, @RequestBody Student student) {
+        Optional<Student> orgStudent = studentRepository.findById(id);
+        if (orgStudent.isPresent()) {
+            student.setId(orgStudent.get().getId());
+            studentRepository.save(student);
+            return ResponseEntity.ok(student);
+        }  else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
 
 
 }
