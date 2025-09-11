bd57a42e770181e5be92f1414e691d429777f0aa
erlmek <erlm@ek.dk>
2025-09-11 10:26:37 +0200

    nu med logging

--- src/main/java/org/example/jpastudente24a/JpaStudentE24AApplication.java
+++ src/main/java/org/example/jpastudente24a/JpaStudentE24AApplication.java
@@ -1,10 +1,18 @@
 package org.example.jpastudente24a;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JpaStudentE24AApplication {
+
+
+    public static final Logger logger = LoggerFactory.getLogger(
+            JpaStudentE24AApplication.class
+    );
+
 
     public static void main(String[] args) {
         SpringApplication.run(JpaStudentE24AApplication.class, args);
